3ba8eb8372fbbb0efc1505c41edc312011c621f2
codeExcalibur <excalibur_zm@hotmail.com>
2016-03-16 01:28:17 +0900

    2

--- JS1.js
+++ JS1.js
@@ -2,5 +2,9 @@
  * Created by excal on 2016/1/7.
  */
 document.writeln("Hello,world!");
-window.open("","new","height=141,width=693,top=10,left=20");
+function ad(){
+    window.open("Test1.html","Test1","height=141,width=693,top=100,left=200");
+}
+setTimeout(ad,5000);
+
 
